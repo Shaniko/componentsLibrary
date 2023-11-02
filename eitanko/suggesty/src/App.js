0c760289c2bf94944793454473373b71048f0531
shani koren <shanikoren@supercalifragilisticexpialidocious.local>
2023-11-02 15:10:56 +0200

    adding mui and test

---
+++ eitanko/suggesty/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { Accordion, Box, Button, Table } from '@mui/material';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button >Button example</Button>
+        <Box>Hi Eitan</Box>
+        <Table></Table>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
